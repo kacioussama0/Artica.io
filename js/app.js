4d6da7c4108b44f57fd5bd717b428756275e18ac
KACI OUSSAMA <73916588+kacioussama0@users.noreply.github.com>
2021-12-29 18:03:29 -0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,10 @@
+let menu = document.querySelector('div.burger-menu');
+let ul = document.querySelector('div.links ul');
+menu.onclick = () => {
+    if(ul.style.display == "flex"){
+        ul.style.display = "none";
+    }
+
+    else 
+    ul.style.display = "flex";
+}
